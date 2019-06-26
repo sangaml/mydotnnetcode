3fad80172c83d2495db2c5896c242de6266af60e
Sangam <36110390+sangaml@users.noreply.github.com>
2019-06-26 14:00:55 +0530

    Update Startup.cs

--- WebApplication3/Startup.cs
+++ WebApplication3/Startup.cs
@@ -27,7 +27,7 @@ namespace WebApplication3
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!!!!");
+                await context.Response.WriteAsync("Hello World!");
             });
         }
     }
